06646f5d87d4f145f4e66003e1e1b2ec726b554a
Duviel Garcia <duviel7@gmail.com>
2018-03-23 15:06:27 -0500

    fixed import module rxjs

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers} from '@angular/http';
 import "rxjs/add/operator/map";
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from './global';
 
 @Injectable()
